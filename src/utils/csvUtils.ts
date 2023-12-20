24547c4da61938828fbce6f94f0668735166052c
Luan Vendt <luan.vendt@bitize.com.br>
2023-12-20 17:50:58 -0300

    Alterando nome do arquivo csv

--- src/utils/csvUtils.ts
+++ src/utils/csvUtils.ts
@@ -6,9 +6,23 @@ export const csvFileFilter = (req, file, callback) => {
     callback(null, true);
 };
 export const csvFileName = (req, file, callback) => {
-    //const name = file.originalname.split('.')[0];
+    // //const name = file.originalname.split('.')[0];
+    // const fileExtName = extname(file.originalname);
+    // callback(null, `data${}${fileExtName}`);
+    // Obter a data e hora atual
+    const agora = new Date();
+
+    const ano = agora.getFullYear();
+    const mes = String(agora.getMonth() + 1).padStart(2, '0');
+    const dia = String(agora.getDate()).padStart(2, '0');
+    const hora = String(agora.getHours()).padStart(2, '0');
+    const minuto = String(agora.getMinutes()).padStart(2, '0');
+
     const fileExtName = extname(file.originalname);
-    callback(null, `data${fileExtName}`);
+
+    const fileName = `data-${ano}${mes}${dia}-${hora}${minuto}${fileExtName}`;
+
+    callback(null, fileName);
 };
 export const getCSVFile = () => {
     //const name = file.originalname.split('.')[0];
